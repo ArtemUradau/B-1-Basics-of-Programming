f830274fd4b46459e26bcf34530545056045acf4
ArtemUradau <art.urad1998@gmail.com>
2018-12-09 19:14:13 +0300

    Добавьте файлы проекта.

---
+++ B-1-Basics-of-Programming/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace B_1_Basics_of_Programming
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            const int adultage = 18;
+
+            string name = "Ola";
+            var surname = "Rondarava";
+
+            int age = 17;
+
+            string[] documents = new string[]
+            {
+                "Application",
+                "Permission"
+            };
+
+            if (age >= adultage)
+            {
+                Console.WriteLine($"{name} go to window 5");
+            }
+            else
+            {
+                bool hasPermission = documents.Contains("Permission");
+
+                if (hasPermission)
+                {
+                    Console.WriteLine($"{name} go to window 5");
+                    Console.WriteLine(name + " go to window 5");
+                }
+                else
+                {
+                    Console.WriteLine($"{name} go home");
+                }
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
